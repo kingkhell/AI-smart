d84081b7557cae318fa37e7a2988a103427b0bb7
King khell <128979851+kingkhell@users.noreply.github.com>
2024-10-15 09:25:35 +0530

    dfs 1a.py

--- dfs 1a.py
+++ dfs 1a.py
@@ -1,116 +1,23 @@
-Python 3.12.7 (v3.12.7:0b05ead877f, Sep 30 2024, 23:18:00) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+graph = {
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-following is the depth first search
-5
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 10, in <module>
-    dfs(visited,graph,'5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 6, in dfs
-    visited.append(node)
-AttributeError: 'set' object has no attribute 'append'
+'5': ['3','7'], '3': ['2', '4'], '7': ['8'], '2': [], '4': ['8'], '8': []
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-following is the depth first search
-5
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 10, in <module>
-    dfs(visited,graph,'5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 7, in dfs
-    for n  in garph[node]:
-NameError: name 'garph' is not defined. Did you mean: 'graph'?
+}
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-following is the depth first search
-5
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 10, in <module>
-    dfs(visited,graph,'5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 7, in dfs
-    for neighbour  in garph[node]:
-NameError: name 'garph' is not defined. Did you mean: 'graph'?
+visited = set() 
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-following is the depth first search
-5
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 10, in <module>
-    dfs(visited,graph,'5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 7, in dfs
-    for neighbour  in garph[node]:
-NameError: name 'garph' is not defined. Did you mean: 'graph'?
+def dfs(visited, graph, node): 
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-following is the depth first search
-5
-3
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 10, in <module>
-    dfs(visited,graph,'5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 8, in dfs
-    dfs(visited,graph,neighbour)
-  File "/Users/zeeshankhan/Documents/hh.py", line 7, in dfs
-    for neighbour  in graph[node]:
-KeyError: '3'
+if node not in visited:
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-following is the depth first search
-5
-3
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 10, in <module>
-    dfs(visited,graph,'5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 8, in dfs
-    dfs(visited,graph,neighbour)
-  File "/Users/zeeshankhan/Documents/hh.py", line 7, in dfs
-    for neighbour in graph[node]:
-KeyError: '3'
+print (node)
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-following is the depth first search
-5
-3
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 10, in <module>
-    dfs(visited,graph,'5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 8, in dfs
-    dfs(visited,graph,neighbour)
-  File "/Users/zeeshankhan/Documents/hh.py", line 7, in dfs
-    for neighbour in graph[node]:
-KeyError: '3'
+visited.add(node)
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-Following is the depth first search
-5
-3
-Traceback (most recent call last):
-  File "/Users/zeeshankhan/Documents/hh.py", line 12, in <module>
-    dfs(visited, graph, '5')
-  File "/Users/zeeshankhan/Documents/hh.py", line 9, in dfs
-    dfs(visited, graph, neighbour)
-  File "/Users/zeeshankhan/Documents/hh.py", line 8, in dfs
-    for neighbour in graph[node]:
-KeyError: '3'
+for neighbour in graph[node]:
 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-Following is the depth first search
-5
->>> 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-Following is the Depth-First Search
-5
-3
-2
-4
-8
-7
->>> 
-= RESTART: /Users/zeeshankhan/Documents/hh.py
-Following is the Depth-First Search
-5
-3
-2
-4
-8
-7
+dfs(visited, graph, neighbour)
+
+print("Following is the Depth-First Search")
+
+dfs(visited, graph, '5')
